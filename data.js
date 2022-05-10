6fbfd983fd7360e5459da43f22aa6e33c7345c4d
KASHI TAMANG <tamang.kashi@gmail.com>
2022-05-10 12:37:28 -0700

    adds render function and findbyID function

--- data.js
+++ data.js
@@ -1,17 +1,20 @@
 export const planets = [
     {
+        id: '1',
         name: 'mars',
         image: './assets/mars.jpg',
         distance: '4th',
         materials: ['nickel', 'iron', 'silicon'],
     },
     {
+        id: '2',
         name: 'neptune',
         image: './assets/neptune.jpg',
         distance: '8th',
         materials: ['hydrogen', 'helium', 'nitrogen', 'methane'],
     },
-    {
+    {   
+        id: '3',
         name: 'mercury',
         image: './assets/mercury.jpg',
         distance: '1st',
